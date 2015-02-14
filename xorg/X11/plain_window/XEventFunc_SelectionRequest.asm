996542c6705cb6527072370fb2ea741016cb3597
NlCK <nickaizuddin93@gmail.com>
2015-02-14 21:22:43 +0800

    Update

---
+++ xorg/X11/plain_window/XEventFunc_SelectionRequest.asm
@@ -0,0 +1,28 @@
+;        1         2         3         4         5         6         7
+;234567890123456789012345678901234567890123456789012345678901234567890
+;=====================================================================
+;
+; data_kernel.inc
+;
+; This header file contains structures that are related to
+; Linux Kernel systemcalls.
+;
+;=====================================================================
+
+section .text
+
+XEventFunc_SelectionRequest:
+; **************************************** SelectionRequest Event ****
+; READ( socketX, ESI, 31 )
+    mov    eax, _SYSCALL_READ_
+    mov    ebx, [socketX]
+    lea    ecx, [XEvent_SelectionRequest]
+    mov    edx, 31
+    int    0x80
+; WRITE( stdout, EDI, evtmsg_len )
+    mov    eax, _SYSCALL_WRITE_
+    mov    ebx, _STDOUT_
+    lea    ecx, [evtmsg_SelectionRequest]
+    mov    edx, [evtmsg_len]
+    int    0x80
+    jmp    mainloop
